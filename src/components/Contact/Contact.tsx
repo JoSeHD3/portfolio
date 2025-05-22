930fa3444a9da044f4871abe3e1367b0f2430688
JoSeHD3 <sebastian.bozek3@gmail.com>
2025-05-22 20:45:37 +0200

    Fixed links in Contact component

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,20 +1,29 @@
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import MailIcon from '@mui/icons-material/Mail';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import IconButton from '@mui/material/IconButton';
+import { Link } from '@mui/material';
 
 const Contact = () => {
     return (
         <section className="w-full container mt-20 mb-20 mx-auto flex flex-row align-middle justify-center">
-            <IconButton size="large" className="flex justify-center w-1/3">
+            <Link
+                href="https://www.linkedin.com/in/sebastianbozek3"
+                className="flex justify-center w-1/3"
+            >
                 <LinkedInIcon fontSize="large" className="text-[#a8a8a8]" />
-            </IconButton>
-            <IconButton size="large" className="flex justify-center w-1/3">
+            </Link>
+            <Link
+                href="https://github.com/JoSeHD3"
+                className="flex justify-center w-1/3"
+            >
                 <GitHubIcon fontSize="large" className="text-[#a8a8a8]" />
-            </IconButton>
-            <IconButton size="large" className="flex justify-center w-1/3">
+            </Link>
+            <Link
+                href="mailto:sebastian.bozek3@gmail.com"
+                className="flex justify-center w-1/3"
+            >
                 <MailIcon fontSize="large" className="text-[#a8a8a8]" />
-            </IconButton>
+            </Link>
         </section>
     );
 };
